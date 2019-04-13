30cde890df54a3d29cc2c17e0f79be7bae22a5ee
padalarij <48649319+padalarij@users.noreply.github.com>
2019-04-13 13:47:53 +0300

    Выпадающие менюшки в header теперь работают

--- scripts/mainscript.js
+++ scripts/mainscript.js
@@ -1,37 +1,107 @@
 var body = $("body");
-var first_click=true;
-function DeleteLists() {
-    $(".list").click(function () {
-        $(".list").css({ "display": "none" });
-        click=0;
-    })
-    body.scroll(function () {
-        $(".list").css({ "display": "none" });
-        click=0;
-    })
+function Delete(list) {
+    list.css({ "display": "none" });
 }
-function ShowElementClicks(button, list) {
-    button.click(function () {
-        list.css({ "display": "block", "position": "absolute", "background": "#0d3351", "padding-top": "15px", "padding-left": "15px", "padding-right": "15px" });
-    });
-    return true;
+function Add(list) {
+    list.css({ "display": "block", "position": "absolute", "background": "#0d3351", "padding-top": "15px", "padding-left": "15px", "padding-right": "15px", "border": "10px double #33c7fd", "padding-bottom": "10px" });
 }
 
-var arrdatalist = [$(".mainbutton"), $(".hero__mainlist"), $(".lk"), $(".hero__userlist"), $(".rouble"), $(".hero__roublelist"), $(".language"), $(".hero__languagelist")]
-window.onload = function () {
-    for (i = 0; i < 8; i=i+2) {
-        if (ShowElementClicks(arrdatalist[i],arrdatalist[i+1]))
-        {
-            first_click=false;
+var arrdatalist = [$(".mainbutton"), $(".hero__mainlist"), $(".lk"), $(".hero__userlist"), $(".rouble"), $(".hero__roublelist"), $(".language"), $(".hero__languagelist")];
+var firstclick = true;
+arrdatalist[0].click(
+    function () {
+        body.scroll(
+            function () {
+                Delete(arrdatalist[1]);
+                firstclick = true;
+            }
+        );
+        arrdatalist[1].click(
+            function () {
+                Delete(arrdatalist[1]);
+                firstclick = true;
+            }
+        )
+        if (firstclick == true) {
+            Add(arrdatalist[1]);
+            firstclick = false;
+        }
+        else {
+            Delete(arrdatalist[1]);
+            firstclick = true;
         }
     }
-    var sortbutton = $(".sort");
-    var sortlist = $(".content__sortlist");
-    sortbutton.click(function () {
-        sortlist.css({ "display": "block", "position": "relative", "background": "#0d3351", "padding-top": "15px", "padding-left": "15px", "padding-right": "15px" });
-        first_click=false;
-    })
-    if (first_click == false) {
-        first_click=DeleteLists();
+);
+arrdatalist[2].click(
+    function () {
+        body.scroll(
+            function () {
+                Delete(arrdatalist[3]);
+                firstclick = true;
+            }
+        );
+        arrdatalist[3].click(
+            function () {
+                Delete(arrdatalist[3]);
+                firstclick = true;
+            }
+        )
+        if (firstclick == true) {
+            Add(arrdatalist[3]);
+            firstclick = false;
+        }
+        else {
+            Delete(arrdatalist[3]);
+            firstclick = true;
+        }
     }
-}
+);
+arrdatalist[4].click(
+    function () {
+        body.scroll(
+            function () {
+                Delete(arrdatalist[5]);
+                firstclick = true;
+            }
+        );
+        arrdatalist[5].click(
+            function () {
+                Delete(arrdatalist[5]);
+                firstclick = true;
+            }
+        )
+        if (firstclick == true) {
+            Add(arrdatalist[5]);
+            firstclick = false;
+        }
+        else {
+            Delete(arrdatalist[5]);
+            firstclick = true;
+        }
+    }
+);
+arrdatalist[6].click(
+    function () {
+        body.scroll(
+            function () {
+                Delete(arrdatalist[7]);
+                firstclick = true;
+            }
+        );
+        arrdatalist[7].click(
+            function () {
+                Delete(arrdatalist[7]);
+                firstclick = true;
+            }
+        )
+        if (firstclick == true) {
+            Add(arrdatalist[7]);
+            firstclick = false;
+        }
+        else {
+            Delete(arrdatalist[7]);
+            firstclick = true;
+        }
+    }
+);
+
